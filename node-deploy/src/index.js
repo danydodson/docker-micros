97510e2050cc0f874489e20a486d66ab53d1e726
Dany Dodson <danydodson@gmail.com>
2020-02-28 04:29:24 -0600

    reformatted

--- node-deploy/src/index.js
+++ node-deploy/src/index.js
@@ -1,104 +1,104 @@
 #!/usr/bin/env node
 
-const AWS = require("aws-sdk");
-const child_process = require("child_process");
-const { format } = require("date-fns");
-const fs = require("fs");
-const path = require("path");
-const util = require("util");
+const AWS = require("aws-sdk")
+const child_process = require("child_process")
+const { format } = require("date-fns")
+const fs = require("fs")
+const path = require("path")
+const util = require("util")
 
-const deploymentDir = process.argv[2];
-const deploymentDirName = path.basename(deploymentDir);
+const deploymentDir = process.argv[2]
+const deploymentDirName = path.basename(deploymentDir)
 
-const rel = relPath => path.resolve(deploymentDir, relPath);
+const rel = relPath => path.resolve(deploymentDir, relPath)
 
-const tfFilePath = rel("../terraform/terraform.tfstate");
+const tfFilePath = rel("../terraform/terraform.tfstate")
 
 if (!fs.existsSync(tfFilePath)) {
-  throw new Error('Terraform state file does not exist! Have you run "terraform apply"?');
+  throw new Error('Terraform state file does not exist! Have you run "terraform apply"?')
 }
 
-const { outputs } = JSON.parse(fs.readFileSync(tfFilePath, "utf-8"));
+const { outputs } = JSON.parse(fs.readFileSync(tfFilePath, "utf-8"))
 
-require("dotenv").config({ path: rel("./.deploy.env") });
+require("dotenv").config({ path: rel("./.deploy.env") })
 
-const accessEnv = require("./helpers/accessEnv");
+const accessEnv = require("./helpers/accessEnv")
 
-const exec = util.promisify(child_process.exec);
+const exec = util.promisify(child_process.exec)
 
-const getFullDate = () => format(new Date(), "yyyyMMddHHmmss");
+const getFullDate = () => format(new Date(), "yyyyMMddHHmmss")
 
-const APPLICATION_NAME = accessEnv("APPLICATION_NAME");
+const APPLICATION_NAME = accessEnv("APPLICATION_NAME")
 
-const MAX_BUFFER_SIZE = 1024 * 1024; // 1 MiB
+const MAX_BUFFER_SIZE = 1024 * 1024 // 1 MiB
 
-const awsRegion = outputs["aws-region"].value;
+const awsRegion = outputs["aws-region"].value
 
 const codeDeployClient = new AWS.CodeDeploy({
   accessKeyId: accessEnv("AWS_ACCESS_KEY_ID"),
   apiVersion: "2014-10-06",
   region: awsRegion,
   secretAccessKey: accessEnv("AWS_ACCESS_KEY_SECRET")
-});
+})
 
 const s3Client = new AWS.S3({
   accessKeyId: accessEnv("AWS_ACCESS_KEY_ID"),
   endpoint: new AWS.Endpoint(`https://s3.${awsRegion}.amazonaws.com/`),
   s3ForcePathStyle: true,
   secretAccessKey: accessEnv("AWS_ACCESS_KEY_SECRET")
-});
+})
 
 const rootDir = rel("../");
 
 (async () => {
-  console.log("Deploying in 3 seconds...");
-  await new Promise(resolve => setTimeout(resolve, 3000));
+  console.log("Deploying in 3 seconds...")
+  await new Promise(resolve => setTimeout(resolve, 3000))
 
-  const lockFilePath = rel("../deploy.lock");
-  console.log("Checking for lockfile...");
+  const lockFilePath = rel("../deploy.lock")
+  console.log("Checking for lockfile...")
   if (fs.existsSync(lockFilePath)) {
-    console.error("Lockfile deploy.lock found! Halting...");
-    process.exit();
+    console.error("Lockfile deploy.lock found! Halting...")
+    process.exit()
   }
 
-  console.log("Creating lockfile...");
+  console.log("Creating lockfile...")
   fs.writeFileSync(
     lockFilePath,
     "This stops node-deploy from running concurrently with itself. Remove this if node-deploy complains."
-  );
+  )
 
-  console.log("Checking environment...");
+  console.log("Checking environment...")
   if (!fs.existsSync(rel(".production.env"))) {
-    console.error("No .production.env found! Halting...");
-    process.exit();
+    console.error("No .production.env found! Halting...")
+    process.exit()
   }
 
-  console.log("Copying appspec...");
-  fs.copyFileSync(rel("./appspec.yml"), rel("../appspec.yml"));
+  console.log("Copying appspec...")
+  fs.copyFileSync(rel("./appspec.yml"), rel("../appspec.yml"))
 
-  console.log("Generating deployment file...");
-  const filename = `${deploymentDirName}-deployment-${getFullDate()}.zip`;
-  const zipPath = `/tmp/${filename}`;
+  console.log("Generating deployment file...")
+  const filename = `${deploymentDirName}-deployment-${getFullDate()}.zip`
+  const zipPath = `/tmp/${filename}`
   await exec(
     `zip -r ${zipPath} . -x terraform/\\* -x node_modules/\\* -x \\*/node_modules/\\* -x \\*/.cache/\\* -x .git/\\* -x \\*.DS_Store`,
     { cwd: rootDir, maxBuffer: MAX_BUFFER_SIZE }
-  );
+  )
 
-  console.log("Uploading deployment file...");
+  console.log("Uploading deployment file...")
   await s3Client
     .putObject({
       Body: fs.createReadStream(zipPath),
       Bucket: outputs[`${APPLICATION_NAME}-deployment-bucket-name`].value,
       Key: filename
     })
-    .promise();
+    .promise()
 
-  console.log("Upload complete! Deployment file is", filename);
+  console.log("Upload complete! Deployment file is", filename)
 
-  console.log("Removing deployment file...");
-  fs.unlinkSync(zipPath);
+  console.log("Removing deployment file...")
+  fs.unlinkSync(zipPath)
 
-  console.log("Deploying application...");
+  console.log("Deploying application...")
 
   await codeDeployClient
     .createDeployment({
@@ -113,11 +113,11 @@ const rootDir = rel("../");
         }
       }
     })
-    .promise();
+    .promise()
 
-  console.log("Deployment initiated on CodeDeploy!");
+  console.log("Deployment initiated on CodeDeploy!")
 
-  console.log("Cleaning up...");
-  fs.unlinkSync(rel("../deploy.lock"));
-  fs.unlinkSync(rel("../appspec.yml"));
-})();
+  console.log("Cleaning up...")
+  fs.unlinkSync(rel("../deploy.lock"))
+  fs.unlinkSync(rel("../appspec.yml"))
+})()
