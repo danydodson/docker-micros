2699dd8f1807031de6e76db1064d3696337b410e
Dany Dodson <danydodson@gmail.com>
2020-02-28 01:55:35 -0600

    changed instance ami-id

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -5,7 +5,7 @@ resource "aws_eip" "api-gateway-eip" {
 module "api-gateway" {
   source = "./node-server"
 
-  ami-id               = "ami-0e2ff28bfb72a4e45" // For Amazon Linux AMI 2018.03.0 use ami-07ebfd5b3428b6f4d
+  ami-id               = "ami-0a887e401f7654935" // Amazon Linux 2 AMI (HVM), SSD Volume Type
   iam-instance-profile = module.api-gateway-codedeploy.iam-instance-profile
   key-pair             = aws_key_pair.docker-micros-key.key_name
   name                 = "api-gateway"
